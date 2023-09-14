9f0bfe06eec6ba950120cdc0a85e51de8f040fa5
theredhood427 <bangsil.ronrusselle@auf.edu.ph>
2023-09-14 22:47:38 +0800

    first commit

---
+++ Bangsil_Module01_Activity01/Bangsil_Module01_Activity01/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bangsil_Module01_Activity01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void OpenMainPage(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+        private async void OpenPage2(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
